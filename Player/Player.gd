dc47df6fe8485c84b8283ba0937c3b2e5517b9ac
Ildar Farkhutdinov <farhutir@gmail.com>
2020-06-22 15:01:56 +0300

    autotile/collision

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,9 @@ const MAX_SPEED = 80
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
-onready var animationPlayer = $AnimationPlayer
+#onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,17 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		elif input_vector.x < 0:
-			animationPlayer.play("RunLeft")
-		elif input_vector.y > 0:
-			animationPlayer.play("RunDown")
-		elif input_vector.y < 0:
-			animationPlayer.play("RunUp")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
